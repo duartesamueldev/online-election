2e0fc7141b5570ae8ce0ed77d41510d41490f9ab
duartesamueldev <duartegithub@gmail.com>
2025-06-23 14:23:14 -0300

    Projeto criado

---
+++ src/main/java/com/duarte/online_election_api/OnlineElectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.duarte.online_election_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineElectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineElectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duarte/online_election_api/OnlineElectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte.online_election_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineElectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
