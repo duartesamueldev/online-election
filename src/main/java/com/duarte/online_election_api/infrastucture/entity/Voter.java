004bb0f207bc8e3b33a67489942b6232e2965571
duartesamueldev <duartegithub@gmail.com>
2025-06-24 13:41:00 -0300

    Change in Repository and application.properties

--- src/main/java/com/duarte/online_election_api/infrastucture/entity/Voter.java
+++ src/main/java/com/duarte/online_election_api/infrastucture/entity/Voter.java
@@ -2,8 +2,14 @@ package com.duarte.online_election_api.infrastucture.entity;
 
 import jakarta.persistence.*;
 
+import javax.annotation.processing.Generated;
+
 @Entity
 @Table(name = "tb_voter")
 public class Voter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
--- src/main/java/com/duarte/online_election_api/infrastucture/repository/VoterRepository.java
+++ src/main/java/com/duarte/online_election_api/infrastucture/repository/VoterRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VoterRepository extends JpaRepository<Long, Voter> {
+public interface VoterRepository extends JpaRepository<Voter, Long> {
 }
