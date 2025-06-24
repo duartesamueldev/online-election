28b767bf0f5fac94ed8f234a0db24aeb2c78a59f
duartesamueldev <duartegithub@gmail.com>
2025-06-24 12:54:20 -0300

    Change in candidateDTO annotations

--- src/main/java/com/duarte/online_election_api/adapters/dtos/CandidateDTO.java
+++ src/main/java/com/duarte/online_election_api/adapters/dtos/CandidateDTO.java
@@ -4,11 +4,7 @@ import com.duarte.online_election_api.business.enums.Gender;
 import com.duarte.online_election_api.business.enums.Nationality;
 import com.duarte.online_election_api.business.enums.PoliticalParty;
 import com.duarte.online_election_api.business.enums.Position;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
-import org.hibernate.loader.ast.spi.NaturalIdLoadOptions;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -18,21 +14,22 @@ public record CandidateDTO(
         @NotBlank(message = "CPF is required")
         String cpf,
         @NotNull(message = "Candidate number is required.")
-        @Size(min = 2, max = 2, message = "Candidate number must contain only 2 digits. Ex: 11, 22, 13")
+        @Min(value = 10, message = "Candidate number must be a 2-digit number, ex: 10, 11, 22")
+        @Max(value = 99, message = "Candidate number must be a 2-digit number, ex: 11, 22, 13")
         Integer number,
-        @NotBlank(message = "State is required")
+        @NotNull(message = "State is required")
         @Size(min = 2, max = 2, message = "State must be informed by the acronym, Ex: RJ")
         String state,
         @NotNull(message = "Birth date is required")
         @Past(message = "Birth date must be in the past.")
         LocalDate birthDate,
-        @NotBlank(message = "Gender is required")
+        @NotNull(message = "Gender is required")
         Gender gender,
-        @NotBlank(message = "nationality is required")
+        @NotNull(message = "nationality is required")
         Nationality nationality,
-        @NotBlank(message = "Political party is required")
+        @NotNull(message = "Political party is required")
         PoliticalParty politicalParty,
-        @NotBlank(message = "Position is required")
+        @NotNull(message = "Position is required")
         Position position
 ) {
 }
